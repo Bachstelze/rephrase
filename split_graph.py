2881764ec068fd48fdbf1bb410d19c40089b63fb
Turan Rustamli (MSc) <s1263388@skaol.inf.ed.ac.uk>
2013-06-29 16:49:32 +0100

    Changed path to match graph location on skaol (split_graph.py)

--- split_graph.py
+++ split_graph.py
@@ -1,14 +1,14 @@
 
 
 def main():
-    f = open('15.graph')
+    f = open('../../evaluation/enru.graph')
     line = f.readline()
     current_sentence_id = ''
     out = None
     while line != '':
         sentence_id = line.split(' ')[0]
         if sentence_id != current_sentence_id:
-            out = open('graph-parts/s' + sentence_id + '.graph', 'w')
+            out = open('../../evaluation/graph-parts/s' + sentence_id + '.graph', 'w')
             current_sentence_id = sentence_id
             pass
 
