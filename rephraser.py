444f9780bfc28903a1d44b764f9c8a8ff703f5f2
Turan <mc2run@gmail.com>
2013-06-29 21:28:18 +0100

    Short list fix

--- rephraser.py
+++ rephraser.py
@@ -78,8 +78,12 @@ def get_paraphrases(intervals, db, n_results):
         paraphrase = ''
         if len(partials) > 0:
             for j in range(0, len(partials)):
+                if i >= len(partials[j]):
+                    paraphrase = ''
+                    break
                 paraphrase += partials[j][i] + ' '
-        paraphrases.append(paraphrase)
+        if paraphrase != '':
+            paraphrases.append(paraphrase)
 
     return paraphrases
 
